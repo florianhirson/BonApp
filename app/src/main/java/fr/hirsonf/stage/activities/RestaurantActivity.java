35b553fef153741c6ea570bd5b7ab8ee4398a317
Florian <flohirson@gmail.com>
2018-01-08 01:25:56 +0000

    Create RestaurantActivity

---
+++ app/src/main/java/fr/hirsonf/stage/activities/RestaurantActivity.java
@@ -0,0 +1,15 @@
+package fr.hirsonf.stage.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.hirsonf.stage.R;
+
+public class RestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+    }
+}
